0f3e5af562342cd727dba9609195c4a627c66b1f
ParthJindal23k <parthjindal23316@gmail.com>
2025-06-11 16:18:02 +0530

    things added

--- frontend/vite-project/src/components/manager/MyExpense/Expense.jsx
+++ frontend/vite-project/src/components/manager/MyExpense/Expense.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 const Expense = () => {
 
     const [data , setData] = useState(null);
+    const [history , sethistory] = useState([])
 
     useEffect(() =>{
         const fetchData = async () =>{
@@ -12,6 +13,14 @@ const Expense = () => {
                 const email = localStorage.getItem('email')
                 const res = await axios.post('http://localhost:8000/api/manager/dashboard/', {email:email} )
                 setData(res.data)
+                
+                const histres = await axios.post('http://localhost:8000/api/expense-history/', {
+                    email:email
+                })
+
+                sethistory(histres.data)
+
+
             } catch (error) {
                 console.log("Error in Fetching Data", error);
 
@@ -51,10 +60,34 @@ const Expense = () => {
             <h3 className='font-semibold'>Rejected</h3>
             <p>Amount</p>
         </div>
-
-
-
       </div>
+
+      <div className="mt-20 bg-white p-4 rounded-lg shadow ml-4 mr-5">
+      <h2 className="mt-2 text-lg font-semibold mb-4">Reimbursement History</h2>
+      <table className="w-full">
+        <thead>
+          <tr className="bg-gradient-to-r from-pink-300 to-pink-800 text-transparent bg-clip-text">
+            <th className='pb-2'>Date</th>
+            <th className="pb-2">Expense Request Date</th>
+            <th className="pb-2">Category</th>
+            <th className="pb-2">Amount</th>
+            <th className="pb-2">Status</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          {history.map((data , id) => (
+            <tr>
+                <td>Date</td>
+                <td>{data.date}</td>
+                <td>{data.note}</td>
+                <td>{data.amount}</td>
+                <td>{data.status}</td>
+            </tr>
+          )   )}
+        </tbody>
+      </table>
+    </div>
 
 
 
