164fbee591ae34b5c4653ccfe05c082e7b2e87bb
Krish181105 <krish812garg@gmail.com>
2025-06-17 18:07:09 +0530

    Docker Init

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -5,8 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
-
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',   // 👈 Allow connections from outside Docker
+    port: 5173,        // 👈 Match this with docker-compose.yml
+    strictPort: true   // 👈 Prevent Vite from picking another port if 5173 is taken
+  }
 })
